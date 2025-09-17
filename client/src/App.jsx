e2a3c432c28b95df96df0a239da745b8d383a082
Bilal <mbilalvisions2@gmail.com>
2025-09-17 04:03:28 -0700

    install react js and tailwindcss and create the first templet

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-amber-300 font-bold ">
+        Hi, Bilal Abdul Khaliq
+      </h1>
+      <h2>How are you? Hope so you are fine</h2>
+    </div>
+  );
+}
