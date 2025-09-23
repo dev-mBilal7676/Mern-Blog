289d17da667c37e3e33c4832978bf5c1cb0a2bdb
Bilal <mbilalvisions2@gmail.com>
2025-09-23 07:46:58 -0700

    project setup install react tailwind and succesfully connects mongodb atlas

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.listen(5000, () => {
+  console.log("server is running on port 5000 !");
+});
