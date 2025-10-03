ee79f3f269e2219d580f9f604282abdd785d7b42
Bilal <m.bilalvisions2@gmail.com>
2025-10-03 08:37:17 -0700

    create sign in pagr UI and add Redux toolkit

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,46 @@
 import React from "react";
-import { Label, TextInput, Button } from "flowbite-react";
-// import { useState } from 'react';
-import { Link } from "react-router-dom";
+import { Label, TextInput, Button, Alert, Spinner } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 // import OAuth from "../components/OAuth";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -24,8 +60,8 @@ export default function SignUp() {
 
         {/* right */}
 
-        <div className="flex-1">
-          <form className="flex flex-col gap-4">
+        <div className="flex-1 ">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your username" />
               <h2>Your username </h2>
@@ -33,7 +69,7 @@ export default function SignUp() {
                 type="text"
                 placeholder="Username"
                 id="username"
-                // onChange={handleChange}
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -43,7 +79,7 @@ export default function SignUp() {
                 type="email"
                 placeholder="name@company.com"
                 id="email"
-                // onChange={handleChange}
+                onChange={handleChange}
               />
             </div>
 
@@ -54,23 +90,23 @@ export default function SignUp() {
                 type="password"
                 placeholder="Password"
                 id="password"
-                // onChange={handleChange}
+                onChange={handleChange}
               />
             </div>
+            
             <Button
               className="px-4 py-2 rounded-lg text-white bg-gradient-to-r from-purple-500 via-purple-500 to-blue-500 hover:opacity-90"
               type="submit"
-              // disabled={loading}
+              disabled={loading}
             >
-              Sign Up
-              {/* {loading ? (
+              {loading ? (
                 <>
-                  <Spinner size='sm' />
-                  <span className='pl-3'>Loading...</span>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
                 </>
               ) : (
-                'Sign Up'
-              )} */}
+                "Sign Up"
+              )}
             </Button>
             {/* <OAuth /> */}
           </form>
@@ -80,11 +116,11 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
-          {/* {errorMessage && (
+          {errorMessage && (
             <Alert className="mt-5" color="failure">
               {errorMessage}
             </Alert>
-          )} */}
+          )}
         </div>
       </div>
     </div>
