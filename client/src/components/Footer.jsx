d7caedfc4f163772be683da53a22f3e235158f03
Bilal <m.bilalvisions2@gmail.com>
2025-10-03 08:36:10 -0700

    create Footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,94 @@
+import {
+  Footer,
+  FooterCopyright,
+  FooterDivider,
+  FooterIcon,
+  FooterLink,
+  FooterLinkGroup,
+  FooterTitle,
+} from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsGithub,
+  BsDribbble,
+} from "react-icons/bs";
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-8 border-blue-700">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Bilal's
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <FooterTitle title="About" />
+              <FooterLinkGroup col>
+                <FooterLink
+                  href="https://www.100jsprojects.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  100 JS Projects
+                </FooterLink>
+                <FooterLink
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Bilal's Blog
+                </FooterLink>
+              </FooterLinkGroup>
+            </div>
+            <div>
+              <FooterTitle title="Follow us" />
+              <FooterLinkGroup col>
+                <FooterLink
+                  href="https://www.github.com/sahandghavidel"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </FooterLink>
+                <FooterLink href="#">Discord</FooterLink>
+              </FooterLinkGroup>
+            </div>
+            <div>
+              <FooterTitle title="Legal" />
+              <FooterLinkGroup col>
+                <FooterLink href="#">Privacy Policy</FooterLink>
+                <FooterLink href="#">Terms &amp; Conditions</FooterLink>
+              </FooterLinkGroup>
+            </div>
+          </div>
+        </div>
+        <FooterDivider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <FooterCopyright
+            href="#"
+            by="Bilal's blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <FooterIcon href="#" icon={BsFacebook} />
+            <FooterIcon href="#" icon={BsInstagram} />
+            <FooterIcon href="#" icon={BsTwitter} />
+            <FooterIcon href="#" icon={BsGithub} />
+            <FooterIcon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
